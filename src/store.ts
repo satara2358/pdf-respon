688a8235d7715d16e85b50bb0201b1fb44d95768
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-02-13 22:47:54 -0500

    update

--- src/store.ts
+++ src/store.ts
@@ -1,32 +1,30 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store'
 
 export const APP_STATUS = {
   INIT: 0,
   LOADING: 1,
   CHAT_MODE: 2,
-  ERROR: -1,
-};
+  ERROR: -1
+}
 
-export const appStatus = writable(APP_STATUS.INIT);
-export const appStatusInfo = writable({ id: "", url: "", pages: 0 });
+export const appStatus = writable(APP_STATUS.INIT)
+export const appStatusInfo = writable({
+  id: '907fc982732cea3bad7d7d3ccaa01cc4',
+  url: 'https://res.cloudinary.com/midudev/image/upload/v1706810969/pdf/907fc982732cea3bad7d7d3ccaa01cc4.pdf',
+  pages: 4
+})
 
 export const setAppStatusLoading = () => {
-  appStatus.set(APP_STATUS.LOADING);
-};
+  appStatus.set(APP_STATUS.LOADING)
+}
 
 export const setAppStatusError = () => {
-  appStatus.set(APP_STATUS.ERROR);
-};
+  appStatus.set(APP_STATUS.ERROR)
+}
 
-export const setAppStatusChatMode = ({
-  id,
-  url,
-  pages,
-}: {
-  id: string;
-  url: string;
-  pages: number;
-}) => {
-  appStatus.set(APP_STATUS.CHAT_MODE);
-  appStatusInfo.set({ id, url, pages });
-};
+export const setAppStatusChatMode = (
+  { id, url, pages } :
+  { id: string, url: string, pages: number }) => {
+  appStatus.set(APP_STATUS.CHAT_MODE)
+  appStatusInfo.set({ id, url, pages })
+}
