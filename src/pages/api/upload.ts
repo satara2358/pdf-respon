bc651fcd71512f22cb7a4ec84f0575f8e3a27993
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-02-13 22:21:52 -0500

    OpenApi

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,22 +1,25 @@
 import type { APIRoute } from "astro";
-
+import fs from "node:fs/promises";
 import { v2 as cloudinary, type UploadApiResponse } from "cloudinary";
+import path from "node:path";
 
 cloudinary.config({
   cloud_name: "dyftajet0",
   api_key: "983936734227578",
-  api_secret: import.meta.env.CLOUDINARY_SECRET
+  api_secret: import.meta.env.CLOUDINARY_SECRET,
 });
+
+const outputDir = path.join(process.cwd(), "public/text");
 
 const uploadStream = async (
   buffer: Uint8Array,
   options: {
     folder: string;
+    ocr: string;
   }
 ): Promise<UploadApiResponse> => {
   return new Promise((resolve, reject) => {
-    cloudinary
-      .uploader
+    cloudinary.uploader
       .upload_stream(options, (error, result) => {
         if (result) return resolve(result);
         reject(error);
@@ -38,17 +41,30 @@ export const POST: APIRoute = async ({ request }) => {
 
   const result = await uploadStream(uint8Array, {
     folder: "pdf",
+    ocr: "adv_ocr",
   });
 
-  const {
-    asset_id: id,
-    secure_url :url,
-    pages
-  } = result
+  const { asset_id: id, secure_url: url, pages, info } = result;
 
-  return new Response(JSON.stringify({
-    id, 
-    url,
-    pages
-  }))
+  const data = info?.ocr?.adv_ocr?.data;
+  const text = data
+    .map((blocks: { textAnnotations: { description: string }[] }) => {
+      const annotations = blocks["textAnnotations"] ?? {};
+      const first = annotations[0] ?? {};
+      const content = first["description"] ?? "";
+      return content.trim();
+    })
+    .filter(Boolean)
+    .join("\n");
+
+  fs.writeFile(`${outputDir}/${id}.text`, text, "utf-8");
+
+  return new Response(
+    JSON.stringify({
+      id,
+      url,
+      pages,
+    })
+  );
 };
+
